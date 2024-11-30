079fef353d5062322b1b4c8bd1cdd82cb389481a
BocoTravis <x00209463@mytudublin.ie>
2024-11-30 12:57:10 +0000

    Added CRUD functionality such as editing and delting ONLY ADMIN can edit and delete then added create new product function

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from.models import Category, Product
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from .forms import ProductForm  # 
 
 def prod_list(request, category_id=None):
     category = None
@@ -10,7 +11,7 @@ def prod_list(request, category_id=None):
         category = get_object_or_404(Category, id=category_id)
         products = Product.objects.filter(category=category, available=True)
     
-    paginator = Paginator(products,6) 
+    paginator = Paginator(products, 6) 
     try:
         page = int(request.GET.get('page', '1'))
     except ValueError:
@@ -20,11 +21,37 @@ def prod_list(request, category_id=None):
     except (EmptyPage, InvalidPage):
         products = paginator.page(paginator.num_pages)
 
-        
-    return render(request, 'shop/category.html',{'category':category, 'prods':products})
-    
-    
+    return render(request, 'shop/category.html', {'category': category, 'prods': products})
 
 def product_detail(request, category_id, product_id):
     product = get_object_or_404(Product, category_id=category_id, id=product_id)
-    return render(request, 'shop/product.html', {'product':product})
+    return render(request, 'shop/product.html', {'product': product})
+
+# CRUD Views
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('shop:all_products')
+    else:
+        form = ProductForm()
+    return render(request, 'shop/product_form.html', {'form': form, 'title': 'Add Product'})
+
+def product_edit(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('shop:all_products')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'shop/product_form.html', {'form': form, 'title': 'Edit Product'})
+
+def product_delete(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('shop:all_products')
+    return render(request, 'shop/product_confirm_delete.html', {'product': product})
